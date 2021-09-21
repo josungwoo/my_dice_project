d812540b848a9e6b276ab8751ad16e9532ea90fc
FlutLab.io <>
2021-09-21 14:31:17 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,9 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+              child: FlatButton(
+                child: Image.asset('images/dice1.png'),
+              ),
             ),
           ),
         ],
