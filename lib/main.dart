6f5306e15e48da7a36e049dd4ce9b26ad968842b
FlutLab.io <>
2021-09-23 14:14:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _DicePageState extends State<DicePage> {
                   rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
-              child: Image.asset('images/dice$rightDiceNumber.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
         ],
